073f83212ad238c9d6eb669f8b7bb8862931b2af
codejm <codejm@163.com>
2015-09-01 10:00:01 +0800

    yaf resque,fileupload bug

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -11,15 +11,6 @@ class IndexController extends \Core_BaseCtl {
 
     // 默认Action
     public function indexAction() {
-        Resque::setBackend($this->config['resque']['redis']['host']);
-        $args = array(
-            'time' => time(),
-            'argarr' => array(
-                'test' => 'test'
-            )
-        );
-        $jobId = Resque::enqueue('default', 'JobFirst', $args, true); // JobFirst 任务处理类, 返回任务id
-        echo 'Queued job:'.$jobId;
         phpinfo();
         exit;
     }
